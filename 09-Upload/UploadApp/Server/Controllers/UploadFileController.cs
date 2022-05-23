3b3ab92cbdcde0f18b63c4c155122da7ae774ae9
Andrea Tosato <andrea.tosato@hotmail.it>
2022-05-23 20:18:54 +0000

    Upload di un file

---
+++ 09-Upload/UploadApp/Server/Controllers/UploadFileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UploadApp.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+        [Consumes("multipart/form-data")]
+        [HttpPost("single")]
+        public async Task<IActionResult> UploadSingle(IFormFile file)
+        {
+            var name = file.FileName;
+            var size = file.Length;
+            return Ok(new
+            {
+                FileName = name,
+                Size = size,
+            });
+        }
+
+
+    }
+}
